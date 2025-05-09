ee357ab5f105170f4c62550d10be12e62e3ad84c
Beedrick <pettric.m@yahoo.com>
2025-05-09 15:11:20 +1200

    config db connection for backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { DB_URI } from './config'; 
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(DB_URI),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
